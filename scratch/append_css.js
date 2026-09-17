60bb8d2407fd1c7cb866489d73d1a46177280f92
yunus9 <yunus@bilesimkimya.com>
2026-09-17 16:41:14 +0300

    Remove unnecessary scratch folder

--- scratch/append_css.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('css/style.css', 'utf8');
-content += `\n.news-sidebar {\n    flex: 0 0 320px;\n}\n@media (max-width: 1024px) {\n    .news-sidebar {\n        flex: 1 1 100%;\n        max-width: 100%;\n    }\n}\n`;
-fs.writeFileSync('css/style.css', content);
-console.log('Appended successfully');
